f7c5e58c1401922821f69882f449f9fa43b84939
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-09-20 20:59:05 -0400

    fix latest time

--- remark-modified-time.mjs
+++ remark-modified-time.mjs
@@ -1,10 +1,15 @@
+/**
+ * Copyright 2025 Colton Loftus
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import { execSync } from "child_process";
 
 // this plugin adds last modified time to frontmatter
 // https://docs.astro.build/en/recipes/modified-time/
 export function remarkModifiedTime() {
   return function (tree, file) {
-    const filepath = file.history[0];
+    const filepath = file.path; // instead of file.history[0]
     const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
     file.data.astro.frontmatter.lastModified = result.toString();
   };
